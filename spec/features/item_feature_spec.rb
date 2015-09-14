facc0df07176b8f7ac53aaaf25eed790958be187
Katya-yani Vyas <katyayani.vyas@gmail.com>
2015-09-14 19:36:19 +0100

    Started trying to upload items

---
+++ spec/features/item_feature_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'items' do
+  context 'no items have been added' do
+    scenario 'should display a prompt to add an item' do
+      visit '/items'
+      expect(page).to have_content 'No items yet'
+      expect(page).to have_link 'Add an item'
+    end
+  end
+end
