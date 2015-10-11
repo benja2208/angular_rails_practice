fd17239478ad63c5bbb6bd66050e001b806c02fc
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-10-11 12:56:03 +0100

    adding items with no picture

--- spec/features/item_feature_spec.rb
+++ spec/features/item_feature_spec.rb
@@ -8,4 +8,21 @@ feature 'items' do
       expect(page).to have_link 'Add an item'
     end
   end
+
+  context 'adding items' do 
+  	scenario 'User can add a photo with a description, a price and a store' do 
+  		visit '/items'
+  		click_link 'Add an item' 
+  		fill_in 'Description', with: 'my first photo'
+  		fill_in 'Price', with: '12'
+  		select 'Zara', from: 'Store'
+  		click_button 'Add item'
+  		expect(current_path).to eq('/items')
+  		expect(page).to have_css ('img[src*="tomato.jpg"]')
+  		expect(page).to have_content 'my first photo'
+  		expect(page).to have_content '£12.00'
+  		expect(page).to have_content 'Zara'
+  	end 
+
+  end 
 end
