f703dad2e56367e471fade8c0cf6914eb7ce5715
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-10-12 22:05:34 +0100

    started facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {registration: 'registrations'}
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registration => 'registrations' }
+
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
