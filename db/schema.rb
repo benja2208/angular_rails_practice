07b7276010957e126585f5255b7dcf5fde619a10
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-10-12 20:25:26 +0100

    fixed no given price issue

--- db/schema.rb
+++ db/schema.rb
@@ -51,13 +51,11 @@ ActiveRecord::Schema.define(version: 20151011154836) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "username"
     t.string   "user_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   add_foreign_key "comments", "items"
 end
