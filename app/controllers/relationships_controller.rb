3a4e9c68ddf2db89fceca8266b80ac0a0e35db9b
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-10-25 14:30:13 +0000

    ajax following

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,13 +3,21 @@ class RelationshipsController < ApplicationController
 
 	def create
 		user = User.find(params[:followed_id])
-		current_user.follow(user)
+		current_user.follow(@user)
+		respond_to do |format|
+			format.html { redirect_to @user}
+			format.js
+		end 
 		redirect_to user
 	end 
 
 	def destroy
 		user = Relationship.find(params[:id]).followed
-		current_user.unfollow(user)
+		current_user.unfollow(@user)
+		respond_to do |format|
+			format.html {redirect_to @user}
+			format.js
+		end 
 		redirect_to user
 	end 
 
@@ -17,6 +25,30 @@ class RelationshipsController < ApplicationController
     unless user_signed_in?
       flash[:danger] = "Please log in."
       redirect_to login_url
+    end
+  end
+end
+
+
+
+class RelationshipsController < ApplicationController
+  before_action :logged_in_user
+
+  def create
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
+  end
+
+  def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
     end
   end
 end
